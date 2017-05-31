383a9f1a6427afed4e9a4a7484aa537d557906f5
Lucas Lima <lucaslimapoa2@gmail.com>
2017-05-30 23:03:37 -0300

    Removing Example files, to force linguist to run again

--- Example/Pods/Target Support Files/Pods-NewsAPISwift_ExampleTests/Pods-NewsAPISwift_ExampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NewsAPISwift_ExampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_NewsAPISwift_ExampleTests
-@end
--- Example/Pods/Target Support Files/Pods-NewsAPISwift_Tests/Pods-NewsAPISwift_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NewsAPISwift_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_NewsAPISwift_Tests
-@end
