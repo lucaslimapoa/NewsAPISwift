f1a4fe4cc2ad9eb55917a7b4bb4f2f1b610e4278
Lucas Lima <lucaslimapoa2@gmail.com>
2018-06-21 22:20:46 -0300

    project: adds new project.

---
+++ Source/Supporting Files/NewsAPISwift.h
@@ -0,0 +1,19 @@
+//
+//  NewsAPISwift.h
+//  NewsAPISwift
+//
+//  Created by Lucas Lima on 21/06/18.
+//  Copyright © 2018 Lucas Lima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NewsAPISwift.
+FOUNDATION_EXPORT double NewsAPISwiftVersionNumber;
+
+//! Project version string for NewsAPISwift.
+FOUNDATION_EXPORT const unsigned char NewsAPISwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewsAPISwift/PublicHeader.h>
+
+
