5b5bd732b3f88650b535c8f60d603274018bd667
Lucas Lima <lucaslimapoa2@gmail.com>
2018-06-21 23:21:11 -0300

    project: adds Cocoapods to NewsAPISwift project along with Test dependencies.

---
+++ Tests/NewsAPIProviderSpec.swift
@@ -0,0 +1,17 @@
+//
+//  Tests.swift
+//  Tests
+//
+//  Created by Lucas Lima on 21/06/18.
+//  Copyright © 2018 Lucas Lima. All rights reserved.
+//
+
+import Quick
+import Nimble
+import NewsAPISwift
+
+class NewsAPIProviderSpec: QuickSpec {
+    override func spec() {
+        
+    }
+}
